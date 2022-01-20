2a62d1ac943686682525b8ffd3ee3bd84469a2e4
Jean Paul Ari Maldonado <jean.ari@ucsp.edu.pe>
2022-01-20 12:54:16 -0500

    corrección de duplicado.

--- app.js
+++ app.js
@@ -11,14 +11,26 @@ document.addEventListener('DOMContentLoaded' ,()=>{
             name:'ice-cream',
             img: 'images/ice-cream.png'
         },{
-            name:'blank',
-            img: '/images/blank.png'
-        },{
             name:'pizza',
             img: 'images/pizza.png'
         },{
-            name:'white',
-            img: 'images/white.png'
+            name:'milkshake',
+            img: 'images/milkshake.png'
+        }, {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },{
+            name:'fries',
+            img: 'images/fries.png'
+        },{
+            name:'hotdog',
+            img: 'images/hotdog.png'
+        },{
+            name:'ice-cream',
+            img: 'images/ice-cream.png'
+        },{
+            name:'pizza',
+            img: 'images/pizza.png'
         },{
             name:'milkshake',
             img: 'images/milkshake.png'
